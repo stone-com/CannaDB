d07c34cdf3e3d719ea2634399cbcfb30dca06f42
jakesagewrite-commits <jakesagewrite@gmail.com>
2026-03-14 12:21:53 -0700

    Jakes comment part 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
     strains: true,
     harvestReport: false,
   });
-  // This is Jake's comment
+  // This is Jake's comment part 2 electric boogaloo
   // Tracks which open windows are currently minimized to the taskbar.
   // When a key is true, that window's DraggableWindow renders null and a tab
   // appears in the taskbar at the bottom of the screen instead.
